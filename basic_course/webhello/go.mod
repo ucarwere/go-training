420d07f44df28f4e9734834c1da427236e518543
vankichi <kyukawa315@gmail.com>
2020-03-25 18:58:11 +0900

    :sparkles: webHW
    Signed-off-by: vankichi <kyukawa315@gmail.com>

---
+++ basic_course/webhello/go.mod
@@ -0,0 +1,3 @@
+module github.com/vankichi/go-training/basic_course/webhello
+
+go 1.14
---
+++ basic_course/webhello/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello world")
+}
+
+func main () {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
