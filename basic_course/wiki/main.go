5ef8b4c61496d4157f4e1c2d0b73314f7d89235d
vankichi <kyukawa315@gmail.com>
2020-04-13 01:52:47 +0900

    :sparkles: create view/edit/save handler
    Signed-off-by: vankichi <kyukawa315@gmail.com>

--- basic_course/wiki/main.go
+++ basic_course/wiki/main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
 
-func main () {
-	fmt.Println("this is main func of wiki")
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+const lenPath = len("/view/")
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[lenPath:]
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[lenPath:]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[lenPath:]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
+func (p *Page) save() error {
+	fsname := p.Title + ".txt"
+	return ioutil.WriteFile(fsname, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	fsname := title + ".txt"
+	body, err := ioutil.ReadFile(fsname)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":8080", nil)
 }
